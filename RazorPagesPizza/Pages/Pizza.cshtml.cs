9efd46166ec9cb4bc7d9c4c6d4cfd6968c69de6d
Tuyen Dang <tuyendq@gmail.com>
2022-10-14 19:25:20 +0700

    implement OnGet()

--- RazorPagesPizza/Pages/Pizza.cshtml.cs
+++ RazorPagesPizza/Pages/Pizza.cshtml.cs
@@ -1,15 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
 
 namespace RazorPagesPizza.Pages
 {
     public class PizzaModel : PageModel
     {
+      public List<Pizza> pizzas = new();
+
       [BindProperty]
       public Pizza NewPizza { get; set; }
         public void OnGet()
         {
+          pizzas = PizzaService.GetAll();
         }
+
+      public string GlutenFreeText(Pizza pizza)
+      {
+        return pizza.IsGlutenFree ? "Gluten Free" : "Not Gluten Free";
+      }
     }
 }
