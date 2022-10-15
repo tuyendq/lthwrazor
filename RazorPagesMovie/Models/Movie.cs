a627024df2fb8ca0e947580ef4057df48c99e472
Tuyen Dang <tuyendq@gmail.com>
2022-10-15 19:42:32 +0700

    updage generated code

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -11,6 +11,6 @@ public class Movie
   [DataType(DataType.Date)]
   public DateTime ReleaseDate { get; set; }
   public string Genre { get; set; } = string.Empty;
-  [Column(TypeName = "decimal(18, 2)"]
+  [Column(TypeName = "decimal(18, 2)")]
   public decimal Price { get; set; }
 }
