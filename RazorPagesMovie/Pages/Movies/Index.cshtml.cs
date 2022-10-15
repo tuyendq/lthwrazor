1b2bcdf7207a341a8ab9d9348ef8da3655070890
Tuyen Dang <tuyendq@gmail.com>
2022-10-15 22:38:16 +0700

    Implement search by Genre

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -28,11 +28,23 @@ namespace RazorPagesMovie.Pages_Movies
 
         public async Task OnGetAsync()
         {
+          // Use LINQ to get list of genres
+          IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
           var movies = from m in _context.Movie select m;
           if (!string.IsNullOrEmpty(SearchString))
           {
             movies = movies.Where(s => s.Title.Contains(SearchString));
           }
+
+          if (!string.IsNullOrEmpty(MovieGenre))
+          {
+            movies = movies.Where(x => x.Genre == MovieGenre);
+          }
+
+          Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
 
           Movie = await movies.ToListAsync();
 
