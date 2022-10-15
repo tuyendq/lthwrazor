493702a94e60919b4251cd8362cfde05fab975a9
Tuyen Dang <tuyendq@gmail.com>
2022-10-15 17:43:15 +0700

    update display name

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models;
 
@@ -6,8 +7,10 @@ public class Movie
 {
   public int ID { get; set; }
   public string Title { get; set; } = string.Empty;
+  [Display(Name = "Release Date")]
   [DataType(DataType.Date)]
   public DateTime ReleaseDate { get; set; }
   public string Genre { get; set; } = string.Empty;
+  [Column(TypeName = "decimal(18, 2)"]
   public decimal Price { get; set; }
 }
