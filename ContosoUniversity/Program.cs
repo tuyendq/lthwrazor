c07c839127c6953cba6d63052d0981267c3d880b
Tuyen Dang <tuyendq@gmail.com>
2022-10-16 09:33:30 +0700

    Add the database exception filer

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<SchoolContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("SchoolContextSQLite") ?? throw new InvalidOperationException("Connection string 'SchoolContextSQLite' not found.")));
 
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +18,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+  app.UseDeveloperExceptionPage();
+  app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
